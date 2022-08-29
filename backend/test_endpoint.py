6b418ea0927e6b166e756d013b81cc25407841e2
0805004949 <no1je1233@gmail.com>
2022-08-29 21:58:28 +0900

    fix: flask current_app instead of using app

--- backend/test_endpoint.py
+++ backend/test_endpoint.py
@@ -41,54 +41,28 @@ def test_ping(api):
     assert b"pong" in resp.data
 
 
-# # login
-# def test_login(api):
-#     # twitter test
-#     # 트위터를 작성 할려면 로그인을 해야하는데
-#     user_info = {"email": "dog-cat@gmail.com", "password": "dog486"}
-#     resp = api.post("/login", data=json.dumps(user_info), content_type="application/json")
+# login
+def test_login(api):
+    # twitter test
+    # 트위터를 작성 할려면 로그인을 해야하는데
+    user_info = {"email": "dog-cat@gmail.com", "password": "dog486"}
+    resp = api.post("/login", data=json.dumps(user_info), content_type="application/json")
 
-#     assert b"access_token" in resp.data
-
-
-# def test_follow(api):
-#     # login to get token
-#     user_info = {"email": "dog-cat@gmail.com", "password": "dog486"}
-#     resp = api.post("/login", data=json.dumps(user_info), content_type="application/json")
-#     access_token = json.loads(resp.data.decode("UTF-8"))["access_token"]
-
-#     # follow
-#     follow_info = {"id": "20", "follow": 13}
-
-#     # see if tweet_list is empty
-#     resp = api.get("/timeline?user_id=20", headers={"Authorization": access_token})
-#     tweets = json.loads(resp.data)
-#     if
-
-#     resp = api.post("/follow", data=json.dumps(follow_info), content_type="application/json", headers={"Authorization": access_token})
-
-#     # 성공했을 경우
+    assert b"access_token" in resp.data
 
 
-# def test_timeline(api):
-#     # login to get token
-#     user_info = {"email": "dog-cat@gmail.com", "password": "dog486"}
-#     resp = api.post("/login", data=json.dumps(user_info), content_type="application/json")
-#     access_token = json.loads(resp.data.decode("UTF-8"))["access_token"]
+def test_tweet(api):
+    # login to get token
+    user_info = {"email": "dog-cat@gmail.com", "password": "dog486"}
+    resp = api.post("/login", data=json.dumps(user_info), content_type="application/json")
+    access_token = json.loads(resp.data.decode("UTF-8"))["access_token"]
 
+    # write tweet
+    contents = {"tweet": f"im writing bullshits!--{user_info['email']}"}
+    resp = api.post("/tweet", headers={"Authorization": access_token}, data=json.dumps(contents), content_type="application/json")
+    assert resp.status_code == 200
 
-# def test_tweet(api):
-#     # login to get token
-#     user_info = {"email": "dog-cat@gmail.com", "password": "dog486"}
-#     resp = api.post("/login", data=json.dumps(user_info), content_type="application/json")
-#     access_token = json.loads(resp.data.decode("UTF-8"))["access_token"]
-
-#     # write tweet
-#     contents = {"tweet": f"im writing bullshits!--{user_info['email']}"}
-#     resp = api.post("/tweet", headers={"Authorization": access_token}, data=json.dumps(contents), content_type="application/json")
-#     assert resp.status_code == 200
-
-#     # check if i can see my latest tweet
-#     resp = api.get("/timeline?user_id=20", headers={"Authorization": access_token})
-#     tweets = json.loads(resp.data)
-#     assert tweets["timeline"][-1]["tweet"] == contents["tweet"]
+    # check if i can see my latest tweet
+    resp = api.get("/timeline?user_id=20", headers={"Authorization": access_token})
+    tweets = json.loads(resp.data)
+    assert tweets["timeline"][-1]["tweet"] == contents["tweet"]
