2b34e0dfb52c0062f1413d3dfa64673be85056c9
0805004949 <no1je1233@gmail.com>
2022-08-29 21:57:54 +0900

    build

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,8 @@
 db = {"user": "x", "password": "x!", "host": "x", "port": "3306", "database": "x"}
+test_db = db = {"user": "x", "password": "x!", "host": "x", "port": "3306", "database": "x"}
 
 DB_URL = f"mysql+pymysql://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
+test_config = {"DB_URL": f"mysql+pymysql://{test_db['user']}:{test_db['password']}@{test_db['host']}:{test_db['port']}/{test_db['database']}?charset=utf8"}
 if __name__ == "__main__":
     from sqlalchemy import create_engine, text
 
